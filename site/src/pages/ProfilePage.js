2fa821f1c76ebf6c9ef833e7c373a8765655212c
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-03-16 07:10:04 -0300

    profile_page_ok

--- site/src/pages/ProfilePage.js
+++ site/src/pages/ProfilePage.js
@@ -7,10 +7,10 @@ import { useContextoTribuna } from '../hooks'
 import Checkboxes  from '../components/Checkbox';
 
 const ProfilePage = () => {
-  const { userData, updateUserData, getClienteById } = useAuth();
-  const { tribuna } = useContextoTribuna()
-  const [ tribunasUsa, setTribunasUsa ] = useState([])
-  const [ novasTribunas, setNovasTribunas ] = useState([])
+  const { userData, updateUserData, getClienteById} = useAuth();
+  const { tribuna } = useContextoTribuna();
+  const [tribunasUsa, setTribunasUsa] = useState([]);
+  const [novasTribunas, setNovasTribunas] = useState([]);
   const [formData, setFormData] = useState({
     nome: '',
     email: '',
@@ -25,42 +25,31 @@ const ProfilePage = () => {
   });
 
   const isChecked = (lista1, lista2) => {
-    lista1.forEach(item1 => {
-      // Verifica se o elemento da primeira lista está presente na segunda lista
-      const encontrado = lista2.some(item2 => item1.id === item2.tribuna.id);
-
-      const idjunt = lista2.find(item3 => item3.tribuna.id === item1.id)
-      // Adiciona o campo de verificação ao objeto JSON
-      item1.checked = encontrado 
-      if (idjunt !== undefined){
-        item1.idjunt = idjunt.id
-      }else{
-        item1.idjunt = -1
+    lista1.forEach((item1) => {
+      const encontrado = lista2.some((item2) => item1.id === item2.tribuna.id);
+      const idjunt = lista2.find((item3) => item3.tribuna.id === item1.id);
+      item1.checked = encontrado;
+      if (idjunt !== undefined) {
+        item1.idjunt = idjunt.id;
+      } else {
+        item1.idjunt = -1;
       }
     });
-
-  // Retorna a primeira lista modificada
-  return lista1;
-  }
+    return lista1;
+  };
 
   const handleCheckboxChange = (id, idjunt) => {
-    setNovasTribunas(prevTribunas => {
-      // Verifica se o id já está na lista
-      const exists = prevTribunas.find(item => item.id === id);
-      
-      // Se o id não existe na lista, adiciona-o
+    setNovasTribunas((prevTribunas) => {
+      const exists = prevTribunas.find((item) => item.id === id);
       if (!exists) {
         return [...prevTribunas, { id: Number(id), idjunt: idjunt }];
       }
-      
-      // Se o id já existe na lista, remove-o
-      return prevTribunas.filter(item => item.id !== id);
+      return prevTribunas.filter((item) => item.id !== id);
     });
   };
 
-
   useEffect(() => {
-    setTribunasUsa(isChecked(tribuna,userData.tribunas))
+    setTribunasUsa(isChecked(tribuna, userData.tribunas));
     const fetchClienteData = async () => {
       try {
         const clienteData = await getClienteById(userData.cliente.id);
@@ -69,75 +58,54 @@ const ProfilePage = () => {
         console.error('Erro ao buscar dados do cliente:', error);
       }
     };
-
     fetchClienteData();
   }, [userData.cliente.id, getClienteById, tribunasUsa, tribuna]);
 
-  //console.log(tribuna);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const tribunasCD = async() => {
-    try{
+  const tribunasCD = async () => {
+    try {
       novasTribunas.map(async (tri) => {
-        if(tri.idjunt === -1){
+        if (tri.idjunt === -1) {
           const response = await fetch('http://localhost:3001/ct/post', {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json;charset=utf-8'
+              'Content-Type': 'application/json;charset=utf-8',
             },
-            body: JSON.stringify({cliente: userData.cliente.id, tribuna: tri.id})
+            body: JSON.stringify({ cliente: userData.cliente.id, tribuna: tri.id }),
           });
           if (response.ok) {
+            logout();
             console.log('Usuário cadastrado com sucesso!');
-            // Recarregar a lista de usuários após cadastrar um novo
           } else {
             console.error('Erro ao cadastrar usuário:', response.statusText);
           }
-        }else{
+        } else {
           const response = await fetch(`http://localhost:3001/ct/delete/${tri.idjunt}`, {
-            method: 'DELETE'
+            method: 'DELETE',
           });
           if (response.ok) {
+            logout();
             console.log('Usuário excluído com sucesso!');
-            // Recarregar a lista de usuários após excluir
           } else {
             console.error('Erro ao excluir usuário:', response.statusText);
           }
         }
-      })
+      });
       const response = await fetch(`http://localhost:3001/ct/allTri/${userData.cliente.id}`, {
         method: 'GET',
         headers: {
-          'Content-Type': 'application/json;charset=utf-8'
-        }
+          'Content-Type': 'application/json;charset=utf-8',
+        },
       });
-      const trib = await response.json()
+      const trib = await response.json();
       updateUserData({ ...userData, cliente: formData, tribunas: trib });
-    }catch(err){
-
-    }
-  }
- 
-  
-console.log(userData.tribunas);
-useEffect(() => {
-  const fetchClienteData = async () => {
-    try {
-      const clienteData = await getClienteById(userData.cliente.id);
-      setFormData(clienteData);
-      // Atualize as tribunasUsa com os dados atualizados do cliente
-      setTribunasUsa(isChecked(tribuna, clienteData.tribunas));
-    } catch (error) {
-      console.error('Erro ao buscar dados do cliente:', error);
-    }
+      logout(); // Chama o logout após a atualização
+    } catch (err) {}
   };
-
-  fetchClienteData();
-}, [userData.cliente.id, getClienteById, tribuna]);
-
 
   const handleUpdatePassword = async () => {
     try {
@@ -158,7 +126,9 @@ useEffect(() => {
       console.error('Erro ao atualizar a senha:', error);
       alert('Erro ao atualizar a senha. Tente novamente mais tarde.');
     }
+    logout(); // Chama o logout após a atualização
   };
+
   const handleUpdateClientData = async () => {
     try {
       const response = await fetch(`/cliente/modify/${userData.cliente.id}`, {
@@ -168,19 +138,30 @@ useEffect(() => {
         },
         body: JSON.stringify(formData),
       });
+  
       if (response.ok) {
-        const updatedClienteData = await response.json();
-        // Atualize apenas os dados do cliente no estado local
-        const updatedUserData = { ...userData, cliente: updatedClienteData };
-        updateUserData(updatedUserData);
+        // Se a resposta estiver ok, exibe uma mensagem de sucesso
         alert('Dados do cliente atualizados com sucesso!');
+        logout();
+        // Atualiza os dados do usuário localmente sem verificar os dados retornados pelo servidor
+        const updatedUserData = { ...userData, cliente: formData };
+        updateUserData(updatedUserData);
+        // Chama o logout após a atualização
+        logout();
       } else {
-        alert('Erro ao atualizar os dados do cliente. Tente novamente mais tarde.');
+      
       }
     } catch (error) {
-      console.error('Erro ao atualizar dados do cliente:', error);
-      alert('Erro ao atualizar os dados do cliente. Tente novamente mais tarde.');
+      
     }
+  };
+  
+  
+  
+  const logout = () => {
+    localStorage.removeItem('userData');
+    // setUserData({ token: null, cliente: null });
+    window.location.href = "/";
   };
 
   return (
