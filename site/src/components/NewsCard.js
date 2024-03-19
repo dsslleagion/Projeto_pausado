03752221e0854139686c1f6213486f09d9747ff0
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-03-19 19:38:49 -0300

    home_update

--- site/src/components/NewsCard.js
+++ site/src/components/NewsCard.js
@@ -1,19 +1,13 @@
-import React, { useState } from 'react';
-import './NewsCard.css';
+// NewsCard.js
+import React from 'react';
 
 const NewsCard = ({ title, content, date }) => {
-  const [expanded, setExpanded] = useState(false);
-
-  const toggleExpand = () => {
-    setExpanded(!expanded);
-  };
-
   return (
-    <div className={`news-card bg-gray-200 rounded shadow p-4 mb-4 ${expanded ? 'expanded' : ''}`} onClick={toggleExpand}>
-      <h3 className="news-title text-xl font-bold mb-2">{title}</h3>
-      <div className="news-image mb-2"></div> {/* Imagem fictícia */}
-      <p className="news-content text-gray-800">{content}</p>
-      <p className="news-date text-gray-500">{date}</p>
+    <div className="news-card">
+      <img src="https://via.placeholder.com/335x100" alt="Imagem Fictícia" />  {/* Adiciona a imagem fictícia */}
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <span>{date}</span>
     </div>
   );
 };
--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import NavigationBar from '../components/NavigationBar';
 import NewsCard from '../components/NewsCard';
-import './Home.css'; // Importe o arquivo CSS da Home
 import Footer from '../components/Footer';
+import './Home.css';
 
 const Home = () => {
   const [news, setNews] = useState([]);
+  const [mainNews, setMainNews] = useState([]);
 
   useEffect(() => {
     getData();
@@ -18,32 +22,55 @@ const Home = () => {
         'Content-Type': 'application/json;charset=utf-8'
       }
     })
-    .then((response) => response.json())
-    .then((data) => {
-      const formattedData = data.map((item) => ({
-        id: item.id,
-        title: item.titulo,
-        content: item.conteudo,
-        date: new Date(item.dataPublicacao).toLocaleDateString(),
-      }));
-      setNews(formattedData);
-    })
-    .catch((error) => {
-      console.error('Erro ao obter notícias:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        const formattedData = data.map((item) => ({
+          id: item.id,
+          title: item.titulo,
+          content: item.conteudo,
+          date: new Date(item.dataPublicacao).toLocaleDateString(),
+        }));
+        setNews(formattedData);
+        setMainNews(formattedData.slice(0, 3));
+
+      })
+      .catch((error) => {
+        console.error('Erro ao obter notícias:', error);
+      });
   }
 
   return (
-    <div>
+    <div className="home-page">
       <NavigationBar />
-      <div className="container mx-auto px-4 py-8 main-container">
-        <h1 className="text-3xl font-bold mb-4">Feed de Notícias</h1>
-        <div className="news-grid">
+      <div className="">
+      <div className="container2">
+        <div className="main-news">
+          <Slider dots infinite autoplay>
+            {mainNews.map((item) => (
+              <div key={item.id} className="carousel-news-item">
+                <img src="https://via.placeholder.com/720x300" alt="Imagem Fictícia" />
+                <h2>{item.title}</h2>
+                <p>{item.content}</p>
+                <span>{item.date}</span>
+              </div>
+            ))}
+          </Slider>
+        </div>
+
+        <div className="secondary-news">
           {news.map((item) => (
             <NewsCard key={item.id} title={item.title} content={item.content} date={item.date} />
           ))}
         </div>
       </div>
+      <div className="container3">
+      <div className="list-news-down">
+          {news.map((item) => (
+            <NewsCard key={item.id} title={item.title} content={item.content} date={item.date} />
+          ))}
+        </div>
+        </div>
+        </div>
       <Footer />
     </div>
   );
