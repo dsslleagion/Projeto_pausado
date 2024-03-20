5c8c3e1f286cce39a2244418105190a37d875263
mateushlsilva <mateushls01@gmail.com>
2024-03-20 15:54:37 -0300

    Finaliza tribunas

--- site/src/pages/TribunasPage.js
+++ site/src/pages/TribunasPage.js
@@ -1,27 +1,46 @@
 // TribunasPage.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './TribunasPage.css';
 import NavigationBar from '../components/NavigationBar';
 import Footer from '../components/Footer';
 
 const TribunasPage = () => {
+  const [tribunas, setTribunas] = useState([]);
+ 
+
+  useEffect(() => {
+    fetchTribunas();
+  }, []);
+
+  const fetchTribunas = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/tribuna/all');
+      if (!response.ok) {
+        throw new Error('Erro ao buscar tribunas');
+      }
+      const data = await response.json();
+      setTribunas(data);
+    } catch (error) {
+      console.error('Erro ao buscar tribunas:', error);
+    }
+  };
+
   return (
-    <div>
+    <div className="">
       <NavigationBar></NavigationBar>
-    <div className="tribunas-page">
+    <div className="tribunas-page container">
       <h1>Tribunas</h1>
       <div className="tribuna">
-        <h2>Educação</h2>
-        <p>Descrição breve sobre a tribuna de Educação.</p>
-        <Link to="/tribuna/educacao">Ver detalhes</Link>
+        {tribunas.map((item) => (
+          <div>
+            <h2>{item.nome}</h2>
+            <p>{item.descricao}</p>
+          </div>
+        ))}
+        
       </div>
-      <div className="tribuna">
-        <h2>Saúde</h2>
-        <p>Descrição breve sobre a tribuna de Saúde.</p>
-        <Link to="/tribuna/saude">Ver detalhes</Link>
-      </div>
-      {/* Adicione mais tribunas conforme necessário */}
+    
     </div>
     <Footer></Footer>
     </div>
