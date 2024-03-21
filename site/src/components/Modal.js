a33bea985ec2f2075d663c5fc02709dcf7c6d451
mateushlsilva <mateushls01@gmail.com>
2024-03-21 17:37:05 -0300

    Adiciona tabela no cadastro e atualização do candidato

--- site/src/components/Modal.js
+++ site/src/components/Modal.js
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+
 import { useState } from 'react';
 
 export function ModalComponent({title, desc}){
@@ -35,6 +36,43 @@ export function ModalComponent({title, desc}){
                     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                         {desc}
                     </Typography>
+                </Box>
+            </Modal>
+        </div>
+    )
+}
+
+export function ModalChildren({ image, children }){
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    return(
+        <div style={{ display: 'inline-flex', alignItems: 'center' }}>
+            <img
+                src={image}
+                alt='modal'
+                style={{ width: "30px", padding: "3px", cursor: "pointer",borderRadius: 50 }}
+                onClick={handleOpen}
+            />
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box sx={style}>
+                    {children}
                 </Box>
             </Modal>
         </div>
